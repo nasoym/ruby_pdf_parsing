fa05cdddd5a142998c64fa28039d731991623890
sinan <sinangoo@gmx.de>
2014-10-22 10:14:25 +0200

    experimenting with the walk method

--- parse_file.rb
+++ parse_file.rb
@@ -3,16 +3,56 @@ require 'rubygems'
 
 require 'pdf-reader'
 
+class CustomReceiver
+
+  attr_reader :callbacks, :text
+  def initialize
+    @callbacks = []
+    @text = ''
+  end
+
+#  def begin_text_object(*args)
+#    puts "args.size:#{args.size}"
+#  end
+
+  def show_text_with_positioning(*args)
+    #puts "show_text_with_positioning:"
+    #puts "args.size:#{args.size}"
+    #puts "#{args.first}"
+
+    args.first.each do |i|
+      if i.is_a?(String)
+        @text += i
+      end
+    end
+    @text += "\n"
+
+  end
+
+end
+
 if ARGV.length >= 1
   filename = ARGV[0]
   puts "opening file: #{filename}"
 
   reader = PDF::Reader.new(filename)
 
-  puts reader.pdf_version
-  puts reader.info
-  puts reader.metadata
-  puts reader.page_count
+  puts "page_count:#{reader.page_count}"
+
+  page = reader.pages[0]
+  receiver = PDF::Reader::RegisterReceiver.new
+  #receiver = CustomReceiver.new
+
+  page.walk(receiver)
+
+  receiver.callbacks.each do |cb|
+    puts cb
+#    if cb[:name] == :show_text_with_positioning
+#      #puts cb[:args]
+#    end
+  end
+
+#  puts "text: #{receiver.text}"
 
 end
 
