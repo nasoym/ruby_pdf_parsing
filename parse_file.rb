7933aaf0066f042b93f0b1b2b1eaa453f8fcac25
sinan <sinangoo@gmx.de>
2014-10-24 20:10:05 +0200

    further experimenting with pdf parsing

--- parse_file.rb
+++ parse_file.rb
@@ -8,38 +8,90 @@ class CustomReceiver
   attr_reader :callbacks, :text
   def initialize
     @callbacks = []
-    @text = ''
+    #@text = ''
+    @text = []
+    @current_row = 0
+    @current_column = 0
+    @current_font = []
+    @encoding = PDF::Reader::Encoding.new(:StandardEncoding)
+  end
+
+  def sort_text()
+    @text.sort!{|a,b| b[:row] <=> a[:row] }
+    @text.each do |i|
+      i[:text].sort!{|a,b| a[:column] <=> b[:column]}
+    end
+  end
+
+  def extract_text()
+    final_text = ''
+    @text.each do |row|
+      row[:text].each do |col|
+        #puts col[:text]
+        final_text += col[:text]
+        #final_text += @encoding.to_utf8(col[:text]).to_s
+      end
+      final_text += "\n"
+    end
+    final_text
   end
 
   def begin_text_object(*args)
-#    puts "args.size:#{args.size}"
-    puts "begin_text_object: #{args.first}"
+    #puts "begin_text_object: #{args}"
   end
 
   def end_text_object(*args)
-    #puts "args.size:#{args.size}"
-    puts "end_text_object: #{args.first}"
-    #@text += "\n"
+    #puts "end_text_object: #{args}"
+  end
+
+  def set_text_font_and_size(*args)
+    #puts "set_text_font_and_size: #{args}"
+    @current_font = args
   end
 
   def set_text_matrix_and_text_line_matrix(*args)
-    puts "set_text_matrix_and_text_line_matrix: #{args.first}"
+    #puts "set_text_matrix_and_text_line_matrix: #{args}"
+    @current_column = args[4]
+    @current_row = args[5]
   end
 
   def set_spacing_next_line_show_text(*args)
-    puts "set_spacing_next_line_show_text: #{args.first}"
+    #puts "set_spacing_next_line_show_text: #{args}"
   end
 
   def show_text_with_positioning(*args)
     #puts "show_text_with_positioning:"
     #puts "args.size:#{args.size}"
-    puts "show_text_with_positioning: #{args.first}"
-
+    #puts "show_text_with_positioning: #{args}"
+    text = ''
     args.first.each do |i|
       if i.is_a?(String)
-        @text += i
+        text += i
       end
     end
+    index = @text.index{|i| i[:row]==@current_row}
+    if index.nil?
+      index = @text.index{|i| i[:row]==@current_row}
+      @text.push(
+        {
+          row: @current_row,
+          text: []
+        }
+      )
+    end
+    index = @text.index{|i| i[:row]==@current_row}
+
+    #if @text[@current_row.to_s].nil?
+      #@text[@current_row.to_s] = []
+    #end
+    #@text[@current_row.to_s].push(
+    @text[index][:text].push(
+      {
+        text: text,
+        font: @current_font,
+        column: @current_column
+      }
+    )
     #@text += "\n"
 
   end
@@ -54,7 +106,7 @@ if ARGV.length >= 1
 
   puts "page_count:#{reader.page_count}"
 
-  page = reader.pages[0]
+  page = reader.pages[2]
   receiver = PDF::Reader::RegisterReceiver.new
   #receiver = CustomReceiver.new
 
@@ -67,7 +119,14 @@ if ARGV.length >= 1
 #    end
   end
 
+  #puts receiver.raw_content
+  #puts page.text
+
+  #receiver.sort_text()
+  #text = receiver.extract_text()
+  #puts text
   #puts "text: #{receiver.text}"
+
 
 end
 
